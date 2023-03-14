bd60c76eafc1ba42a45b0eeeaebb058d2a9a0e9f
csaldivar-astate <60787459+csaldivar-astate@users.noreply.github.com>
2023-03-14 13:59:27 -0500

    Create express-session.d.ts

---
+++ src/types/express-session.d.ts
@@ -0,0 +1,10 @@
+import 'express-session';
+
+declare module 'express-session' {
+  export interface Session {
+    clearSession(): Promise<void>; // DO NOT MODIFY THIS!
+
+    // NOTES: Add your app's custom session properties here:
+   
+  }
+}
