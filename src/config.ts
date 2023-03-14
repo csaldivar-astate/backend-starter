9c8d710204d25c1a98aec7a251f3107a4584ad6f
csaldivar-astate <60787459+csaldivar-astate@users.noreply.github.com>
2023-03-14 13:57:53 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,15 @@
 import dotenv from 'dotenv';
+import { Session } from 'express-session';
 
 dotenv.config();
+
+// Must use a function expression here so that `this` is bound to the Session object
+Session.prototype.clearSession = async function clearSession(): Promise<void> {
+  // Must use an arrow function here so that it does not rebind `this`
+  return new Promise((resolve, reject) => {
+    this.regenerate((err) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+};
