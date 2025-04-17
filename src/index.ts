3ac29ce8dbf607e96e8ae4badc31ab9e084c9fb8
Chris Saldivar <chrisdsaldivar@gmail.com>
2025-04-17 10:18:16 -0500

    express v5 port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
 import './config'; // Load environment variables
-import 'express-async-errors'; // Enable default error handling for async errors
-
-import express from 'express';
-
-const app = express();
-app.use(express.static('public', { extensions: ['html'] }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server started http://localhost:${process.env.PORT}`);
-});
